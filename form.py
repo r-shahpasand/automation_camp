55ea4b249f1588bde27df72dcb5f15d2b74bf89c
Shahpasand <shahpasand.radarft@gmail.com>
2024-05-25 22:15:32 +0430

    replace option with sleep

--- form.py
+++ form.py
@@ -1,12 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from time import sleep
 
 base_url = "https://play1.automationcamp.ir"
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome()
 driver.get(f"{base_url}/forms.html")
 
 python_check = driver.find_element('name', 'python')
@@ -15,6 +12,10 @@ python_check.click()
 check_validate = driver.find_element('id', 'check_validate').text
 assert check_validate == 'PYTHON'
 
+sleep(2)
 hello_text = 'Hello World!'
 driver.find_element('id', 'notes').send_keys(hello_text)
 assert driver.find_element('id', 'area_notes_validate').text == hello_text
+sleep(3)
+
+driver.quit()
