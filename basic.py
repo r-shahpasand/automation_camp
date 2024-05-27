ce35a245e9ae8a88291c36868aa4a9dc38e31770
Shahpasand <shahpasand.radarft@gmail.com>
2024-05-28 00:04:20 +0430

    another approach to define options

--- basic.py
+++ basic.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-options = webdriver.ChromeOptions()
+options = Options()
+# options.add_argument('--incognito')
 options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=options)
