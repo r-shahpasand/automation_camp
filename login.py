1cac4efe6613b16d95ded07089ae47129f5cebb7
Shahpasand <shahpasand.radarft@gmail.com>
2024-06-02 23:14:08 +0430

    add login class and finding login related elements

---
+++ login.py
@@ -0,0 +1,15 @@
+class Login:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_textbox_name = "username"
+        self.password_textbox_name = "password"
+        self.login_button_class = "oxd-button oxd-button--medium oxd-button--main orangehrm-login-button"
+
+    def enter_user_name(self, username):
+        self.driver.find_element('name', self.username_textbox_name).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element('name', self.password_textbox_name).send_keys(password)
+
+    def click_on_login_button(self):
+        self.driver.find_element('class name', self.login_button_class).click() 
