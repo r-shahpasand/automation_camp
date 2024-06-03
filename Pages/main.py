96723dba526feb979a5c219ab69391878701db35
Shahpasand <shahpasand.radarft@gmail.com>
2024-06-03 21:57:20 +0430

    separate locators

--- Pages/main.py
+++ Pages/main.py
@@ -1,7 +1,9 @@
+from locators import *
+
+
 class Main:
     def __init__(self, driver):
         self.driver=driver
-        self.dashboard_label_id = ""
 
     def check_dashboard(self):
-        self.driver.find_element('xpath', "//h6[text()='Dashboard']")
+        self.driver.find_element('xpath', dashboard_label_id)
---
+++ locators.py
@@ -0,0 +1,7 @@
+# Login
+username_textbox_name = "username"
+password_textbox_name = "password"
+login_button_type = "//button[@type='submit']"
+
+# Main
+dashboard_label_id = "//h6[text()='Dashboard']"
