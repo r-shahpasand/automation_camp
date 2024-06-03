adf70f99d63cac0303dd1db537a5b5d2a0abcb49
Shahpasand <shahpasand.radarft@gmail.com>
2024-06-03 22:34:40 +0430

    add unittest

--- Tests/loginTests.py
+++ Tests/loginTests.py
@@ -2,16 +2,28 @@ from Pages.login import Login
 from Pages.main import Main
 from selenium import webdriver
 from time import sleep
+import unittest
 
-driver = webdriver.Chrome()
-driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-driver.implicitly_wait(3)
+class LoginTests(unittest.TestCase):
 
-login = Login(driver=driver)
-main = Main(driver=driver)
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.implicitly_wait(3)
+        cls.driver.maximize_window()
 
-login.enter_user_name('Admin')
-login.enter_password('admin123')
-login.click_on_login_button()
-main.check_dashboard()
-sleep(2)
+    def test_valid_login(self):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        login = Login(driver=self.driver)
+        main = Main(driver=self.driver)
+
+        login.enter_user_name('Admin')
+        login.enter_password('admin123')
+        login.click_on_login_button()
+        main.check_dashboard()
+        sleep(2)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
