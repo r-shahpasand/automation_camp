43c9b7692719cb7b0cef5434418089636a5b6c36
Shahpasand <ra.shahpasand@gmail.com>
2024-06-20 11:49:51 +0430

    different approaches to wait for an action

--- Basics/10_wait.py
+++ Basics/10_wait.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 base_url = "https://play1.automationcamp.ir/expected_conditions.html"
 driver = webdriver.Chrome()
@@ -35,6 +37,67 @@ trigger.location_once_scrolled_into_view
 
 wait_until_element_has_not_an_attribute(By.ID, "enabled_target", "class", "success")
 wait_until_element_has_an_attribute(By.ID, "enabled_target", "class", "danger")
-trigger.click()
-wait_until_element_has_an_attribute(By.ID, "enabled_target", "class", "success")
 
+
+# trigger.click()
+# wait_until_element_has_an_attribute(By.ID, "enabled_target", "class", "success")
+
+def wait_until_element_is_enabled(selector, locator, timeout):
+    for i in range(timeout * 5):
+        try:
+            element = driver.find_element(selector, locator)
+            assert element.is_enabled()
+        except:
+            sleep(0.5)
+
+
+# trigger.click()
+# wait_until_element_is_enabled(By.ID, "enabled_target", 5)
+
+
+# wait until element is visible
+def wait_until_element_is_visible(selector, locator, timeout=5):
+    for i in range(timeout * 5):
+        try:
+            element = driver.find_element(selector, locator)
+            assert element.is_displayed()
+        except:
+            sleep(0.5)
+
+
+# wait until element is invisible
+def wait_until_element_is_not_visible(selector, locator, timeout=5):
+    for i in range(timeout * 5):
+        try:
+            element = driver.find_element(selector, locator)
+            assert not element.is_displayed()
+        except:
+            sleep(0.5)
+
+
+target = driver.find_element(By.ID, "visibility_trigger")
+target.location_once_scrolled_into_view
+target.click()
+wait_until_element_is_visible(By.ID, "visibility_target")
+
+# WebDriverWait until expected condition
+trigger.click()
+wait = WebDriverWait(driver, 5)
+element2 = wait.until(expected_conditions.element_to_be_clickable((By.ID, "enabled_target")))
+print(element2.is_enabled())
+
+
+# wait until page is loaded
+def wait_until_page_is_loaded(timeout=10):
+    for i in range(timeout * 2):
+        try:
+            state = driver.execute_script("return document.readyState")
+            assert state == 'complete'
+            print(state)
+            return
+        except:
+            sleep(0.5)
+
+
+driver.get("https://archive.org/details/texts")
+wait_until_page_is_loaded()
